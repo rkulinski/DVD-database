628b4e0682d966416095ca12ef9ce32d0f61ae55
kulinskir <robert.kulinski93@gmail.com>
2018-05-07 19:32:39 +0200

    Loading screen

--- mobileapp/myDVDs/src/App.js
+++ mobileapp/myDVDs/src/App.js
@@ -2,52 +2,27 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { StackNavigator } from 'react-navigation';
-import firebase from 'firebase';
 import reducers from './reducers';
 import Tiles from './containers/TilesView';
 import MoviesList from './containers/MoviesList';
 import LoginForm from './containers/LoginForm';
-import firebaseConfig from '../firebase.config';
+import LoadingView from './containers/LoadingView';
 
 
 const store = createStore(reducers);
 
 const Navigator = StackNavigator({
+  LoadingView: { screen: LoadingView },
+  LoginForm: { screen: LoginForm },
   Tiles: { screen: Tiles },
   MoviesList: { screen: MoviesList },
 });
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      LoggedIn: false,
-    };
-  }
-
-  componentWillMount() {
-    firebase.initializeApp(firebaseConfig);
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ loggedIn: true });
-      } else {
-        this.setState({ loggedIn: false });
-      }
-    });
-  }
-
-  renderContent() {
-    if (this.state.loggedIn) {
-      return <Navigator/>;
-    }
-    return <LoginForm/>;
-  }
-
   render() {
     return (
       <Provider store={store}>
-        {this.renderContent()}
+        <Navigator/>
       </Provider>
     );
   }
